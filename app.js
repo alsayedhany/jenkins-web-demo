07396ff9e78bab9cc0639ff3dc17c0990f12f1c9
Al-Sayed Hany <eng.elsayedhany@gmail.com>
2025-11-05 19:09:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello first project!');
+    res.send('Hello first project web application!');
 });
 
 app.listen(PORT, () => {
